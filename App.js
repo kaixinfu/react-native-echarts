ef37d6fd907892ae029b228345111c45adf29f3e
liukaixin01 <1360517661@qq.com>
2017-10-24 23:49:10 +0800

    add native-echarts

--- App.js
+++ App.js
@@ -1,57 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Platform
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+import Echarts from 'native-echarts';
+import Dimensions from 'Dimensions';
+const {width} = Dimensions.get('window');
+export default class App extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			apple:[2, 4, 7, 2, 2, 7, 13, 16],
+			organ: [6, 9, 9, 2, 8, 7, 17, 18],
+		}
+	}
+	
+	render() {
+		const option = {
+			//点击某一个点的数据的时候，显示出悬浮窗
+			tooltip : {
+				trigger: 'axis'
+			},
+			//可以手动选择现实几个图标
+			legend: {
+				data:['苹果','橘子']
+			},
+			//各种表格
+			toolbox: {
+				//改变icon的布局朝向
+				//orient: 'vertical',
+				show : true,
+				showTitle:true,
+				feature : {
+					//show是否显示表格，readOnly是否只读
+					dataView : {show: true, readOnly: false},
+					magicType : {
+						//折线图  柱形图    总数统计 分开平铺
+						type: ['line', 'bar','stack','tiled'],
+					},
+					
+				}
+			},
+			xAxis : [
+				{
+					//就是一月份这个显示为一个线段，而不是数轴那种一个点点
+					boundaryGap:true,
+					type : 'category',
+					name : '时间',
+					data : ['1月','2月','3月','4月','5月','6月','7月','8月']
+				}
+			],
+			yAxis : [
+				{
+					type : 'value',
+					name : '销量(kg)'
+				}
+			],
+			//图形的颜色组
+			color:['rgb(249,159,94)','rgb(67,205,126)'],
+			//需要显示的图形名称，类型，以及数据设置
+			series : [
+				{
+					name:'苹果',
+					//默认显
+					type:'bar',
+					data:this.state.apple
+				},
+				{
+					name:'橘子',
+					type:'bar',
+					data:this.state.organ
+				}
+			]
+		};
+		return (
+			<View style={styles.container}>
+				<View style={styles.titleView}>
+					<Text style={styles.title}>统计图</Text>
+				</View>
+				
+				<Echarts option={option} height={800} width={width}/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex:1,
+	},
+	
+	titleView:{
+		height:Platform.OS=='ios'?64:44,
+		paddingTop:Platform.OS=='ios'?14:0,
+		backgroundColor:'#ff6400',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title:{
+		color:'white',
+		fontSize:30,
+		textAlign:'center',
+	},
 });
--- README.md
+++ README.md
@@ -1,41 +1,19 @@
+# react-native-echarts
 
-## React Native Seed v1.0.0
+## Contains
 
-### A [React Native](https://facebook.github.io/react-native/docs/getting-started.html) Starter Kit with [NativeBase](https://nativebase.io/) + [React Navigation](https://reactnavigation.org/) + [MobX](https://github.com/mobxjs/mobx) Apps (iOS & Android)
+- [x] [react-native](http://reactnative.cn/)
+- [x] [native-echarts](https://www.npmjs.com/package/native-echarts)
+- [x] native-echarts Demo
 
-*Brought to you by [GeekyAnts](https://geekyants.com/)*
+## Install
 
+> `git clone git@github.com:liukaixin01/react-native-echarts.git`
 
-## Get Started
+> `npm install`
 
-### 1. System Requirements
+## Dev
 
-* Globally installed [node](https://nodejs.org/en/)
+> ios `react-native run-ios`
 
-* Globally installed [react-native CLI](https://facebook.github.io/react-native/docs/getting-started.html)
-
-
-### 2. Installation
-
-On the command prompt run the following commands
-
-```sh
-$ git clone -b RN https://github.com/GeekyAnts/react-native-boilerplate-mobx-flow.git
-
-$ cd react-native-boilerplate-mobx-flow/
-
-$ npm install
-  or
-  yarn
-```
-
-### Run on iOS
-
-*	Run `react-native run-ios` in your terminal
-
-### Run on Android
-
-*	Run `react-native run-android` in your terminal
-
-
-For apps with more advance designs, please visit **[NativeBase Market](https://market.nativebase.io/)**.
+> android `react-native run-android`
